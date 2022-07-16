21ca82fdd036c93294cf617388ff575d17076ba2
carapaceonbear <seerhan17@gmail.com>
2022-07-16 12:20:54 +0100

    die made

--- scenes/game.gd
+++ scenes/game.gd
@@ -3,24 +3,34 @@ extends Spatial
 onready var camera = $Camera
 onready var cursor = $Cursor
 
+onready var die = preload("res://scenes/die.tscn")
+
 var mouse_pos = Vector2()
 var point = Vector3()
 var z_depth = 8
 signal fire_click(boolean)
 enum States {ACTIVE, INACTIVE}
 var current_state = States.ACTIVE
+var squares = [[],[],[],[],[]]
+var square_groups = ["A_squares", "B_squares", "C_squares", "D_squares", "E_squares"]
 var grid = [[],[],[],[],[]]
-var grid_groups = ["A_squares", "B_squares", "C_squares", "D_squares", "E_squares"]
+var grid_groups = ["A_grid", "B_grid", "C_grid", "D_grid", "E_grid"]
 
 func _ready():
 #	grid = [[A1,A2,A3,A4,A5],[B1,B2,B3,B4,B5]etc]
 	for n in 5:
-		var row = get_tree().get_nodes_in_group(grid_groups[n])
+		var row = get_tree().get_nodes_in_group(square_groups[n])
 		for square in row:
-			grid[n].append(square)
-#	for element in grid:
-#		for index in element:
-#			print(index)
+			squares[n].append(square)
+		row = get_tree().get_nodes_in_group(grid_groups[n])
+		for position in row:
+			grid[n].append(position)
+	for row in squares:
+		for index in row:
+			print(index)
+	for row in grid:
+		for index in row:
+			print(index)
 
 func _input(event):
 	if(event.is_action_pressed("click")):
