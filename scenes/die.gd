e15ebb14db1788b95a53904724a579c559223c5e
carapaceonbear <seerhan17@gmail.com>
2022-07-16 15:04:01 +0100

    adjacent dice rerolling

--- scenes/die.gd
+++ scenes/die.gd
@@ -4,10 +4,13 @@ var rng = RandomNumberGenerator.new()
 var locked = false
 
 func _ready():
+	reroll()
+
+func tell_rotation():
+	return (self.rotation_degrees)
+
+func reroll():
 	rng.randomize()
 	var torque = Vector3(rng.randf_range(-4, 4), rng.randf_range(-4, 4), rng.randf_range(-4, 4))
 	apply_central_impulse(Vector3(0, 10, 0))
 	apply_torque_impulse(torque)
-
-func tell_rotation():
-	return (self.rotation_degrees)
--- scenes/game.gd
+++ scenes/game.gd
@@ -9,15 +9,14 @@ var mouse_pos = Vector2()
 var point = Vector3()
 var z_depth = 8
 signal fire_click(boolean)
-enum States {ACTIVE, INACTIVE}
-var current_state = States.ACTIVE
+enum States {PLACING, REROLLING, RESOLVING}
+var current_state = States.PLACING
 var squares = [[],[],[],[],[]]
 var square_groups = ["A_squares", "B_squares", "C_squares", "D_squares", "E_squares"]
 var grid = [[],[],[],[],[]]
 var grid_groups = ["A_grid", "B_grid", "C_grid", "D_grid", "E_grid"]
 
 func _ready():
-#	grid = [[A1,A2,A3,A4,A5],[B1,B2,B3,B4,B5]etc]
 	for n in 5:
 		var row = get_tree().get_nodes_in_group(square_groups[n])
 		for square in row:
@@ -26,16 +25,11 @@ func _ready():
 		row = get_tree().get_nodes_in_group(grid_groups[n])
 		for position in row:
 			grid[n].append(position)
-#	for row in squares:
-#		for index in row:
-#			print(index)
-#	for row in grid:
-#		for index in row:
-#			print(index)
 
 func _input(event):
-	if(event.is_action_pressed("click")):
+	if(current_state == States.PLACING and event.is_action_pressed("click")):
 		emit_signal("fire_click", true)
+		current_state = States.REROLLING
 
 func _physics_process(delta):
 	point = _mouse_track()
@@ -57,7 +51,43 @@ func check_squares(name):
 		y += 1
 	var target_in_grid = grid[squares_indices[0]][squares_indices[1]]
 	spawn_die(target_in_grid)
-#	print (target_in_grid.name)
+
+func check_grid(grid_position):
+	var grid_indices
+	var y = 0
+	for row in grid:
+		var x = 0
+		for element in row:
+			if (element == grid_position):
+				grid_indices = [y, x]
+			x += 1
+		y += 1
+	return grid_indices
+
+func check_adjacent_in_grid(indices):
+	var rerolling = false
+	if (indices[0] != 0):
+		var above = grid[indices[0] - 1][indices[1]]
+		if (above.get_child_count() != 0):
+			rerolling = true
+			above.get_children()[0].reroll()
+	if (indices[0] != 4):
+		var below = grid[indices[0] + 1][indices[1]]
+		if (below.get_child_count() != 0):
+			rerolling = true
+			below.get_children()[0].reroll()
+	if (indices[1] != 0):
+		var left = grid[indices[0]][indices[1] - 1]
+		if (left.get_child_count() != 0):
+			rerolling = true
+			left.get_children()[0].reroll()
+	if (indices[1] != 4):
+		var right = grid[indices[0]][indices[1] + 1]
+		if (right.get_child_count() != 0):
+			rerolling = true
+			right.get_children()[0].reroll()
+	return rerolling
+
 
 func spawn_die(grid_position):
 	var new_die = die.instance()
@@ -80,6 +110,16 @@ func get_value_from_rotation(rotation):
 
 func _on_FloorArea_body_entered(body):
 	if(body.has_method("tell_rotation")):
+		if (current_state == States.REROLLING):
+			var grid_position = body.get_parent()
+			var indices = check_grid(grid_position)
+			current_state = States.RESOLVING
+			var rerolling = check_adjacent_in_grid(indices)
+			if (!rerolling):
+				current_state = States.PLACING
 		yield(get_tree().create_timer(1), "timeout")
 		var die_rotation = body.tell_rotation()
 		get_value_from_rotation(die_rotation)
+		if (current_state == States.RESOLVING):
+#			yield(get_tree().create_timer(1), "timeout")
+			current_state = States.PLACING
